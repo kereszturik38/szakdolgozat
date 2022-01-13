8089567df051963e5ab3047a41fc56fcb3d91e08
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-01-13 10:38:31 +0100

    first commit

---
+++ inc/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "localhost";
+$db = "c31h202121";
+$user = "kk";
+$password = "hymWuShlkcIXD5ud";
+
+$conn = new mysqli($server, $user, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+include "inc/db.php";
+
+
+echo "<h1>Hello</h1>";
+
+include "inc/htmlheader.php";
+?>
