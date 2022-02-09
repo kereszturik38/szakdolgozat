39347c8dca2fdd8bd7c3a1c9adf54b0ae2f9a7cb
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-02-09 11:03:52 +0100

    nav fix

--- inc/htmlnav.php
+++ inc/htmlnav.php
@@ -1,4 +1,4 @@
-<!-- Navigation-->
+<div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
         <a class="navbar-brand"><i class="bi-images"></i></a>
@@ -12,32 +12,33 @@
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=search">Search</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=settings">Settings</a>
-                </li>
+                <?php if (isset($_SESSION["loggedIn"])) {  ?>
 
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?page=settings">Settings</a>
+                    </li>
+
+                <?php } ?>
             </ul>
+        </div>
+    </div>
+    <div class="d-flex align-items-center mx-sm-auto">
 
-            <a class="btn btn-success mx-5" href="index.php?page=upload">
+        <?php if (isset($_SESSION["loggedIn"])) { ?>
+
+            <a class="btn btn-primary mx-3" href="index.php?page=bookmarks">
+                Bookmarks <span class="badge badge-light"> <i class="bi-bookmark"></i> </span>
+            </a>
+
+            <a class="btn btn-success mx-3" href="index.php?page=upload">
                 Upload <span class="badge badge-light"> <i class="bi-upload"></i> </span>
             </a>
 
-            <span>
 
-
-                <?php
-
-                if (isset($_SESSION["loggedIn"])){
-                    echo "<i class='bi-person-fill em-1'></i>" . $_SESSION["username"];
-                }
-                
-                else {
-                    echo "<a class='btn btn-secondary' href='index.php?page=login'>Login</a>";
-                }
-
-                ?>
-
-            </span>
-        </div>
+            <span class="mx-3"><i class='bi-person-fill em-1'></i><?php echo $_SESSION["username"]; ?></span>
+        <?php } else { ?>
+            <a class='btn btn-secondary' href='index.php?page=login'>Login</a>
+        <?php } ?>
     </div>
 </nav>
+</div>
