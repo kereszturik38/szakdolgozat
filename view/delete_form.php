e10a7d762e13d2574df0be15015e535a8195d3c7
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-02-15 12:27:42 +0100

    deleteform update

--- view/delete_form.php
+++ view/delete_form.php
@@ -1,9 +1,32 @@
+<div id="status"></div>
 <div class="container">
     <h3>Are you sure you wish to delete this post?</h3>
-    <form>
+    <form id="deleteForm">
         <div class="form-group">
             <label for="password">Enter your password to confirm deletion</label>
             <input type="password" id="password" name="password" class="form-control" required/>
         </div>
+
+        <button class="btn btn-primary mt-5" type="submit" name="submit" value="submit">Yes,delete this post</button>
+
     </form>
 </div>
+
+<script>
+    $("#deleteForm").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url:"ajax/delete_post.php",
+            data:{
+                postid: <?php echo $pid; ?>,
+                uid: <?php echo $request_uid; ?>
+            },
+            success: function(e){
+                
+            }
+
+        })
+    })
+
+</script>
