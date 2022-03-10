88f27bc3c70d42301237fe66767d4fdc4b3981fb
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-03-10 11:31:13 +0100

    db export 03 10

--- sql/c31h202121_03_10_local.sql
+++ sql/c31h202121_03_10_local.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Feb 11. 08:08
+-- Létrehozás ideje: 2022. Már 10. 11:30
 -- Kiszolgáló verziója: 10.4.14-MariaDB
 -- PHP verzió: 7.4.10
 
@@ -55,7 +55,16 @@ CREATE TABLE `bookmarks` (
 --
 
 INSERT INTO `bookmarks` (`uid`, `post_id`, `time_added`) VALUES
-(2331, 1293, '2022-02-10 11:32:20');
+(2331, 1293, '2022-02-14 09:18:43'),
+(2331, 1306, '2022-02-14 10:07:41'),
+(2330, 1306, '2022-02-14 11:38:00'),
+(2331, 1332, '2022-02-24 10:23:21'),
+(2331, 1326, '2022-02-24 10:23:26'),
+(2331, 1325, '2022-02-24 10:23:31'),
+(2331, 1329, '2022-02-24 10:23:58'),
+(2331, 1324, '2022-02-24 10:44:45'),
+(2330, 1333, '2022-03-07 10:26:07'),
+(2331, 1334, '2022-03-07 10:31:08');
 
 -- --------------------------------------------------------
 
@@ -71,6 +80,21 @@ CREATE TABLE `comment` (
   `time_commented` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `uid`, `post_id`, `text`, `time_commented`) VALUES
+(427, 2331, 1293, 'asdwd', '2022-02-14'),
+(428, 2331, 1293, 'asd', '2022-02-14'),
+(429, 2331, 1293, 'asf', '2022-02-14'),
+(430, 2331, 1293, 'ad', '2022-02-14'),
+(431, 2331, 1293, 'awgegh', '2022-02-14'),
+(432, 2331, 1293, 't', '2022-02-14'),
+(433, 2331, 1293, 'asd', '2022-02-14'),
+(435, 2331, 1329, '&lt;b&gt;Test&lt;/b&gt;', '2022-02-24'),
+(436, 2331, 1329, '&lt;a href=&quot;index.php&quot;&gt;Test&lt;/a&gt;', '2022-02-24');
+
 -- --------------------------------------------------------
 
 --
@@ -81,6 +105,7 @@ CREATE TABLE `post` (
   `post_id` int(255) NOT NULL,
   `uid` int(255) NOT NULL,
   `title` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `description` varchar(50) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
   `bookmark_count` int(255) NOT NULL DEFAULT 0,
   `comment_count` int(255) NOT NULL DEFAULT 0,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -92,11 +117,16 @@ CREATE TABLE `post` (
 -- A tábla adatainak kiíratása `post`
 --
 
-INSERT INTO `post` (`post_id`, `uid`, `title`, `bookmark_count`, `comment_count`, `timestamp`, `visible`, `type`) VALUES
-(1291, 2330, 'athen', 0, 0, '2022-02-03 07:09:19', 1, 'image/jpg'),
-(1292, 2330, 'Athen1', 0, 0, '2022-02-03 08:04:24', 1, 'image/jpg'),
-(1293, 2330, 'roka', 1, 0, '2022-02-03 08:26:29', 1, 'image/jpg'),
-(1294, 2330, 'Athen3', 0, 0, '2022-02-03 10:05:57', 1, 'image/jpg');
+INSERT INTO `post` (`post_id`, `uid`, `title`, `description`, `bookmark_count`, `comment_count`, `timestamp`, `visible`, `type`) VALUES
+(1293, 2330, 'roka', 'a roka a kamerat nezi', 1, 7, '2022-02-03 08:26:29', 1, 'image/jpg'),
+(1306, 2331, 'first video test', '', 2, 0, '2022-02-14 08:45:06', 1, 'video/mp4'),
+(1324, 2331, 'athen2', '', 1, 0, '2022-02-16 10:11:52', 1, 'image/jpg'),
+(1325, 2331, 'Athen6', '', 1, 0, '2022-02-16 10:12:02', 1, 'image/jpg'),
+(1326, 2331, 'delfinek', '', 1, 0, '2022-02-22 07:50:51', 1, 'image/jpg'),
+(1329, 2331, 'TV', '', 1, 2, '2022-02-22 07:51:25', 1, 'image/jpg'),
+(1332, 2330, 'delfin', '', 1, 0, '2022-02-23 09:32:58', 1, 'image/jpg'),
+(1333, 2330, 'private post', '', 1, 0, '2022-02-23 09:53:19', 0, 'image/png'),
+(1334, 2331, 'private post 2', '', 1, 0, '2022-03-07 09:09:51', 0, 'image/png');
 
 -- --------------------------------------------------------
 
@@ -118,7 +148,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`uid`, `username`, `email`, `password`, `level`) VALUES
 (2330, 'test', 'test@fake.com', '202cb962ac59075b964b07152d234b70', 1),
-(2331, 'random', 'random@fake.com', '250cf8b51c773f3f8dc8b4be867a9a02', 1);
+(2331, 'normal', 'normal@fake.com', '81dc9bdb52d04dc20036dbd8313ed055', 1);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -166,13 +196,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT a táblához `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=424;
+  MODIFY `comment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=437;
 
 --
 -- AUTO_INCREMENT a táblához `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1304;
+  MODIFY `post_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1335;
 
 --
 -- AUTO_INCREMENT a táblához `user`
