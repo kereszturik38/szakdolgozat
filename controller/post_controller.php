fd16e2f64f94eb43c606f06ca24748969a4b15c2
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-03-28 10:41:53 +0200

    wrong URL fix

--- controller/post_controller.php
+++ controller/post_controller.php
@@ -11,31 +11,39 @@ $c = new Comment();
 
 if (isset($_GET["id"])) {
 
-    $p->filterByPID($_GET["id"], $conn);
-
-    $u->filterByUID($p->get_post_uid(), $conn);
-
-    $comments = $c->commentsForPID($_GET["id"], $conn);
-
-    $imgstr = "posts/" . $p->get_post_id() . "-" . $p->get_post_uid() . "/" . $p->get_post_id() . "." . preg_replace("{image/|video/}", "", $p->get_type());
-
-    if (isset($_SESSION["uid"])) {
-        $is_bookmarked = $p->is_bookmarked($_SESSION["uid"], $p->get_post_id(), $conn);
-    } else {
-        $is_bookmarked = false;
-    }
+    if ($p->filterByPID($_GET["id"], $conn)) {
 
 
-    $icon = $is_bookmarked ? 'bi-bookmark-fill ' : 'bi-bookmark ';
 
-    if ($p->get_visible() === 0) {
-        if ((isset($_SESSION["loggedIn"]) && $_SESSION["uid"] === $p->get_post_uid()) || $_SESSION["admin"] === true) {
-            include "view/post.php";
-        } else {
-            header("location: index.php");
+        if ($u->filterByUID($p->get_post_uid(), $conn)) {
+
+            $comments = $c->commentsForPID($_GET["id"], $conn);
+
+            $imgstr = "posts/" . $p->get_post_id() . "-" . $p->get_post_uid() . "/" . $p->get_post_id() . "." . preg_replace("{image/|video/}", "", $p->get_type());
+
+            if (isset($_SESSION["uid"])) {
+                $is_bookmarked = $p->is_bookmarked($_SESSION["uid"], $p->get_post_id(), $conn);
+            } else {
+                $is_bookmarked = false;
+            }
+
+
+            $icon = $is_bookmarked ? 'bi-bookmark-fill ' : 'bi-bookmark ';
+
+            if ($p->get_visible() === 0) {
+                if ((isset($_SESSION["loggedIn"]) && $_SESSION["uid"] === $p->get_post_uid()) || $_SESSION["admin"] === true) {
+                    include "view/post.php";
+                } else {
+                    header("location: index.php");
+                }
+            } else {
+                include "view/post.php";
+            }
+        }else{
+            include "view/404.php";
         }
-    } else {
-        include "view/post.php";
+    }else{
+        include "view/404.php";
     }
 } else {
     header("location: index.php");
--- controller/profile_controller.php
+++ controller/profile_controller.php
@@ -4,39 +4,36 @@ include "model/User.php";
 $u = new User();
 
 
-if(isset($_GET["id"])){
+if (isset($_GET["id"])) {
     $profile = $_GET["id"];
-    
-    if(file_exists("pfp/{$profile}.png")){
+
+    if (file_exists("pfp/{$profile}.png")) {
         $pfp = "pfp/{$profile}.png";
-    }else{
+    } else {
         $pfp = "pfp/default.png";
     }
 
-    $u->filterByUID($profile,$conn);
-    $postcount = $u->get_post_count($conn);
-    include "view/profile.php";
-
-}else if(isset($_SESSION["loggedIn"])){
+    if ($u->filterByUID($profile, $conn)) {
+        $postcount = $u->get_post_count($conn);
+        include "view/profile.php";
+    } else {
+        include "view/404.php";
+    }
+} else if (isset($_SESSION["loggedIn"])) {
     $profile = $_SESSION["uid"];
 
-    if(file_exists("pfp/{$profile}.png")){
+    if (file_exists("pfp/{$profile}.png")) {
         $pfp = "pfp/{$profile}.png";
-    }else{
+    } else {
         $pfp = "pfp/default.png";
     }
 
-    $u->filterByUID($profile,$conn);
-    $postcount = $u->get_post_count($conn);
-    include "view/profile.php";
-}else{
+    if ($u->filterByUID($profile, $conn)) {
+        $postcount = $u->get_post_count($conn);
+        include "view/profile.php";
+    } else {
+        include "view/404.php";
+    }
+} else {
     header('location: index.php');
 }
-
-
-
-
-
-
-
-?>
