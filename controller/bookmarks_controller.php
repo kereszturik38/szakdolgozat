d4c9c09d08e4b2dd3ed695b89c83e3a123e80bf2
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2022-03-30 13:08:08 +0200

    bookmarks login check

--- controller/bookmarks_controller.php
+++ controller/bookmarks_controller.php
@@ -5,36 +5,36 @@ include "model/User.php";
 $p = new Post();
 $u = new User();
 
-if(isset($_GET["pageNum"])){
-    $pageNum = $_GET["pageNum"];
-}else{
-    $pageNum=0;
-   
-}
-$postsPerPage = 6;
-
-$offset = $pageNum * $postsPerPage;
-
-if(isset($_SESSION["uid"])){
-    $uid = strval($_SESSION["uid"]);
-    $numberOfPages = $p->get_number_of_bookmark_pages($postsPerPage,$conn,$uid,0);
-    $bookmarks = $p->get_bookmarks($_SESSION["uid"],$offset,$postsPerPage,$conn);
-    
-    
-    echo "<div class='d-grid gap-3'>";
-    echo "<div class='container px-4 px-lg-5 my-5'>";
-    echo "<div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>";
-    foreach($bookmarks as $bookmark){
-        $imgstr = fetch_file($bookmark);
-        $u->filterByUID($bookmark->get_post_uid(),$conn);
-        include "view/bookmarks.php";
+if (isset($_SESSION["loggedIn"]) && isset($_SESSION["uid"])) {
+    if (isset($_GET["pageNum"])) {
+        $pageNum = $_GET["pageNum"];
+    } else {
+        $pageNum = 0;
     }
-    
-    echo "</div>";
-    echo "</div>";
-    echo "</div>";
-    include "inc/bookmarks_pagination.php";
-    
-}
+    $postsPerPage = 6;
 
-?>
+    $offset = $pageNum * $postsPerPage;
+
+    if (isset($_SESSION["uid"])) {
+        $uid = strval($_SESSION["uid"]);
+        $numberOfPages = $p->get_number_of_bookmark_pages($postsPerPage, $conn, $uid, 0);
+        $bookmarks = $p->get_bookmarks($_SESSION["uid"], $offset, $postsPerPage, $conn);
+
+
+        echo "<div class='d-grid gap-3'>";
+        echo "<div class='container px-4 px-lg-5 my-5'>";
+        echo "<div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>";
+        foreach ($bookmarks as $bookmark) {
+            $imgstr = fetch_file($bookmark);
+            $u->filterByUID($bookmark->get_post_uid(), $conn);
+            include "view/bookmarks.php";
+        }
+
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+        include "inc/bookmarks_pagination.php";
+    }
+} else {
+    header('location: index.php');
+}
